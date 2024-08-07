fcf25f3a39a34e991d862e8f047803cf7437f248
antojk555 <76926446+antojk555@users.noreply.github.com>
2024-08-07 15:25:16 +0530

    new commit

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# CLIENT_ID = os.environ.get('client-id', None)
+# CLIENT_SECRET = os.environ.get('client-secret', None)
+
+CLIENT_ID = "177207201270-igl61uq284s5ehkmn24blms6gsjed21c.apps.googleusercontent.com"
+CLIENT_SECRET = "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"
